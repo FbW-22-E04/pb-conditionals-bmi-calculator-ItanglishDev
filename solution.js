78b89a4327316ce314d038c20dc1deb08bfac2d9
chris <chrishethertonweb@gmail.com>
2022-07-11 14:17:25 +0200

    Q3 to Q5 done

--- solution.js
+++ solution.js
@@ -5,11 +5,24 @@ const johnHeight = 191;
 const johnMass = 88;
 
 //Q2 Calculate both their BMIs and store their BMIs in variables.
-const markBmi = markMass / markHeight ** 2;
-const johnBmi = johnMass / johnHeight ** 2;
+const markBmi = Math.exp(markMass / markHeight);
+const johnBmi = Math.exp(johnMass / johnHeight);
+// console.log(markBmi, johnBmi);
 
 //Q3 Create a boolean variable containing information about whether Mark has a higher BMI than John.
+const markMoreBeefCake = markBmi > johnBmi;
+// console.log(markMoreBeefCake);
 
 //Q4 Print a string to the console containing the variable from step 3 using string interpolation. (e.g. "Is Mark's BMI higher than John's? Why yes, it's [boolean], it is").
 
+console.log(
+  `Q4: Is Mark's BMI higher than John's? Why yes, it's ${markMoreBeefCake}, it is`
+);
+
 //Q5 Create an *if* statement which prints the name and BMI of the person with the highest BMI.
+
+if (markMoreBeefCake === true) {
+  console.log("Q5: ", "Mark", markBmi.toFixed(2));
+} else {
+  console.log("Q5: ", "John", johnBmi.toFixed(2));
+}
